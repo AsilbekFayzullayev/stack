4b2f402380fbb887d1028d22b09a12df80b4279f
Your Name <asilbekfayzullayev1770@gmail.com>
2021-04-29 04:11:18 -0700

    stack

---
+++ layouts/default.vue
@@ -0,0 +1,201 @@
+<template>
+  <v-app dark>
+    <!--    left navigation -->
+    <!--    <v-navigation-drawer-->
+    <!--      v-model="drawer"-->
+    <!--      :mini-variant="miniVariant"-->
+    <!--      :clipped="clipped"-->
+    <!--      fixed-->
+    <!--      app-->
+    <!--    >-->
+    <!--      <v-list>-->
+    <!--        <v-list-item-->
+    <!--          v-for="(item, i) in items"-->
+    <!--          :key="i"-->
+    <!--          :to="item.to"-->
+    <!--          router-->
+    <!--          exact-->
+    <!--        >-->
+    <!--          <v-list-item-action>-->
+    <!--            <v-icon>{{ item.icon }}</v-icon>-->
+    <!--          </v-list-item-action>-->
+    <!--          <v-list-item-content>-->
+    <!--            <v-list-item-title v-text="item.title"/>-->
+    <!--          </v-list-item-content>-->
+    <!--        </v-list-item>-->
+    <!--      </v-list>-->
+    <!--    </v-navigation-drawer>-->
+    <!--  header navbar -->
+    <v-app-bar
+      :clipped-left="clipped"
+      :clipped-right="rightClipped"
+      class="header-nav"
+      fixed
+      app
+    >
+      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="color-white"/>-->
+      <!--      <v-btn-->
+      <!--        icon-->
+      <!--        @click.stop="clipped = !clipped"-->
+      <!--      >-->
+      <!--        <v-icon class="color-white application-icon">mdi-application</v-icon>-->
+      <!--      </v-btn>-->
+      <v-row>
+        <v-col md="8" offset-md="2">
+          <div class="d-flex align-center">
+            <v-toolbar-title v-text="title" class="color-white"/>
+            <v-spacer></v-spacer>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="rightDrawer = true"
+                  icon
+                >
+                  <v-icon color="#fff">
+                    mdi-trophy-variant-outline
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>Spreadsheet</span>
+            </v-tooltip>
+          </div>
+        </v-col>
+      </v-row>
+      <!--      <v-spacer></v-spacer>-->
+      <!--    -->
+      <!--      <v-spacer></v-spacer>-->
+      <!--
+           <span-->
+      <!--        class="advertising"-->
+      <!--        @click.stop="rightDrawer = !rightDrawer"-->
+      <!--      >-->
+      <!--        <span>advertising</span>-->
+      <!--      </span>-->
+    </v-app-bar>
+    <!--   main part  -->
+    <v-main>
+      <v-row class="py-2">
+        <v-col offset-md="2" md="8">
+          <v-row>
+            <v-col cols="12" md="9" sm="8">
+              <v-card outlined class="px-5 py-5">
+                <nuxt/>
+              </v-card>
+            </v-col>
+            <v-col cols="12" md="3" sm="3">
+              <v-card outlined>
+                card
+              </v-card>
+
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-main>
+    <!--        right navigation-->
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :clipped="rightClipped"
+      style=" background-color: #35495e"
+      width="400"
+      right
+      fixed
+
+    >
+      <v-btn icon @click="rightDrawer = false" class="ml-5 mt-5 closeButton">
+        <v-icon>
+          mdi-close
+        </v-icon>
+      </v-btn>
+      <h4 class="text-center py-5 mt-5" style="background-color: #35495e">Users Rating Dashboard</h4>
+      <hr>
+      <v-text-field outlined dense class="mt-2 mx-2" label="search user" color="dark"></v-text-field>
+      <v-data-table
+        :headers="headers"
+        :items="tableItems"
+        :items-per-page="5"
+        class="elevation-1 mx-2"
+      >
+      </v-data-table>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      rightClipped: false,
+      headers: [
+        {text: "№", value: "number"},
+        {text: "Name", value: "name"},
+        {text: "Rate", value: "rate"},
+      ],
+      tableItems: [
+        {number: 1, name: "Asilbek", rate: 100},
+        {number: 2, name: "Ahror", rate: 90},
+      ],
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: true,
+      title: 'StackUz'
+    }
+  },
+  methods: {
+    rightDrawerClick() {
+      alert('message')
+    }
+  }
+}
+</script>
+<style scoped>
+@media (max-width: 1264px) {
+  .application-icon {
+    display: none;
+  }
+}
+
+.closeButton {
+  background-color: #35495e;
+  cursor: pointer;
+  font-size: 30px !important;
+}
+
+.advertising {
+  background-color: black;
+  color: white;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.header-nav {
+  background-color: black !important;
+}
+
+.color-white {
+  color: white !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <!--    <v-row justify="center" align="center">-->
+    <!--      <v-col cols="12" sm="12" md="4">-->
+    <!--        <div class="py-2 d-flex">-->
+    <!--          <h5 class="categoryFront">FrontEnd</h5>-->
+    <!--          <p class="mx-2"></p>-->
+    <!--          <h5 class="categoryBack">Backend</h5>-->
+    <!--        </div>-->
+    <!--      </v-col>-->
+    <!--    </v-row>-->
+    <div class="d-flex">
+      <span style="font-size: 25px">All Questions</span>
+      <v-spacer></v-spacer>
+      <v-btn class="askQuestionBtn">
+        Ask Questions
+      </v-btn>
+    </div>
+    <v-row class="my-5">
+      <v-col cols="12" md="6" sm="6">
+        <h4 class="my-2">1 900 000 questions</h4>
+      </v-col>
+      <v-col cols="12" md="6" sm="6">
+        <v-select
+          label="Search By Category"
+          class="selectCategory"
+          v-model="value"
+          :items="items"
+          item-color="#033"
+          chips
+          dark
+          multiple
+          dense
+          solo
+        ></v-select>
+      </v-col>
+    </v-row>
+    <div>
+      <v-row class="d-flex mt-5" style="outline: 1px solid black;background-color: black" v-for="item of questions"
+             :key="item.id">
+        <v-col cols="12" md="2" sm="2" style="width: 10%" class="px-4 py-1 text-center">
+          <h4 class="text-center" style="font-size: 12px">Username</h4>
+          <img src="https://static.toiimg.com/photo/72975551.cms" width="50" height="50" class="text-center"
+               style="border-radius: 50%"/>
+          <div style="background-color: #35495e;color: white;border-radius: 10px">
+            10
+            <p class="text-center pb-0 mb-0" style="font-size: 12px">answers</p>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="10" md="10" style="width: 85%;border-left: 1px solid #3b8070" class="px-4 py-1">
+          <NuxtLink :to="`/question/${item.title}`" class="title">{{ item.title }}</NuxtLink>
+          <p>{{ item.description }}</p>
+          <div>
+            <span style="background-color: cornflowerblue; padding: 2px 5px">html</span>
+            <span style="background-color: cornflowerblue; padding: 2px 5px">css</span>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ['html', 'css', 'js', 'vue js', 'react js'],
+    value: '',
+    questions: [
+      {
+        userId: "Id",
+        id: "1",
+        title: "title",
+        description: "description ",
+        categories: ['html', 'css', '....'],
+        code: "",
+        answers: [
+          {id: "", description: "answer", code: "", isLiked: false, userId: "id"},
+          {id: "", description: "answer", code: "", isLiked: false, userId: "id"},
+        ]
+      }, {
+        userId: "Id",
+        id: "2",
+        title: "title",
+        description: "description ",
+        categories: ['html', 'css', '....'],
+        code: "",
+        answers: [
+          {id: "", description: "answer", code: "", isLiked: false, userId: "id"},
+          {id: "", description: "answer", code: "", isLiked: false, userId: "id"},
+        ]
+      }
+    ],
+
+    user: {
+      id: "",
+      username: "",
+      email: "",
+      questions: [],
+      answers: [
+        {id: "", description: "answer", code: "", isLiked: false, userId: "id"},
+      ]
+    }
+  }),
+
+  watch: {
+    value(val) {
+      console.log(val)
+    }
+  }
+}
+</script>
+<style scoped>
+.categoryFront {
+  width: 50%;
+  height: 100%;
+  padding: 20px;
+  cursor: pointer;
+  background-color: black;
+  text-align: center;
+}
+
+.categoryBack {
+  width: 50%;
+  padding: 20px;
+  cursor: pointer;
+  background-color: black;
+  text-align: center;
+}
+
+.title {
+  color: cornflowerblue;
+  cursor: pointer;
+}
+
+.askQuestionBtn {
+  background-color: #35495e !important;
+  color: white;
+}
+
+.selectCategory {
+  color: #35495e;
+}
+</style>
